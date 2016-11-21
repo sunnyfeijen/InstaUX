e036c66c13adeea33206ee7016bc72e345215dd5
sunnyfeijen <Sunny@192.168.1.4>
2016-11-21 20:47:40 +0100

    is element in viewport
    Beginning with checking which article is visible in the viewport and
    only applying effects to this article

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 $(document).ready(function() {
 
+// check elements visible on screen //
+$.fn.isOnScreen = function(){
+
+    var win = $(window);
+
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
+
+if ($('.hearticon').isOnScreen()) {
+  console.log("on screen");
+  $('.hearticon').parent().addClass("aaa");
+}
+
+$('article').click(function(){
+    alert($('.hearticon').isOnScreen());
+});
+////////////////////////////////
+
+
 var liked = false;
 
   $(".likebtn").click(function(){
@@ -11,7 +42,6 @@ var liked = false;
 // show heart when icon pressed //
   function showHeart(){
     if(liked){
-
     }
     else{
       $(".likedbtn").show();
